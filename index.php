62fd66974901bbda39dc6c271918587986d5b435
TDS-19 <theodsantos19@gmail.com>
2025-09-01 18:13:16 +0100

    layout raw

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (!$conn) {
     die('Erro na ligação: ' . mysqli_connect_error());
 }
 
-$sql_livros = "SELECT id_livro, titulo, ano, capa FROM livros
+$sql_livros = "SELECT * FROM livros
  ORDER BY ano DESC LIMIT 3";
 $resultado_livros = mysqli_query($conn, $sql_livros);
 
@@ -14,36 +14,40 @@ $sql_autores = "SELECT id_autor, nome, foto, ano_nascimento, nacionalidade, id_l
 $resultado_autores = mysqli_query($conn, $sql_autores);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Author" content="Bernardo e Erica">
-    <link rel="icon" type="image/gif" href="./css/icon/book.webp">
+    <link rel="icon" type="image/gif" href="/css/icon/book.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Roboto+Slab:wght@100;200&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/styles.css">
     <title>Website de Livros</title>
-    </head>
-    <body>
-        <header class="container-fluid">
-        <div class="container-lg">
+</head>
+
+<body>
+
+    <header class="container-fluid" style="background-color: #00481d">
+        <nav class="nav>
+            <div class="container">
             <div class="row align-items-center">
-                <h1 class="col-4">Website de Livros</h1>
+                <h1> Website de Livros</h1>
                 <nav class="col text-end">
-                    <a href="index.php" class="btn btn-warning">Homepage</a>
-                    <a href="livro.php" class="btn btn-warning">Livros</a>
-                    <a href="autor.php" class="btn btn-warning">Autores</a>
+                    <a href="index.php" class="btn btn-warning"> Homepage </a>
+                    <a href="livro.php" class="btn btn-warning"> Livros </a>
+                    <a href="autor.php" class="btn btn-warning"> Autor </a>
                 </nav>
             </div>
-        </div>
+            </div>
+        </nav>
     </header>
+
     <div class="container-lg home">
         <h2>Filmes mais recentes</h2>
         <div class="row">
@@ -53,7 +57,6 @@ $resultado_autores = mysqli_query($conn, $sql_autores);
                     $id = $row['id_livro'];
                     $titulo = htmlspecialchars($row['titulo']);
                     $poster = htmlspecialchars($row['capa']);
-
                     echo <<<HTML
                     <div class="livro-recente-cont col">
                         <div class="livro-recente container" style="background-image: url('$capa');">
