3e22586ca9c5ed9d033c4dfc9127eb698e0634a7
TDS-19 <theodsantos19@gmail.com>
2025-09-04 17:56:27 +0100

    basicos html: incompleto

--- index.php
+++ index.php
@@ -20,11 +20,9 @@ $resultado_autores = mysqli_query($conn, $sql_autores);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Author" content="Bernardo Silvestre e Erica Henrique">
-    <!-- icon -->
     <link rel="icon" type="image/gif" href="./css/icon/arpa.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-     <!-- Link para Fontes externas -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Roboto+Slab:wght@100;200&display=swap" rel="stylesheet">
