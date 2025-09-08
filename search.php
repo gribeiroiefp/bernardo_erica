3fc465d5cd13967d40e152110e17e61b70cb3bce
TDS-19 <theodsantos19@gmail.com>
2025-09-08 17:00:27 +0100

    mentira, faltava o search

--- search.php
+++ search.php
@@ -1,3 +1,12 @@
+<?php
+$conn = mysqli_connect('127.0.0.1', 'root','', 'livros_db');
+if (!$conn) {
+    die('Erro na ligação: ' . mysqli_connect_error());
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
