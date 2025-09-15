870bd35cdc2994d2c9b36eb7b872c612f1cffe57
TDS-19 <theodsantos19@gmail.com>
2025-09-15 16:05:47 +0100

    o bootstrap do comando não estava a funcionar e não tenho tempo, vai a css

--- index.php
+++ index.php
@@ -52,9 +52,7 @@ $resultado_autores = mysqli_query($conn, $sql_autores);
             </table>
         </section>
         <nav>
-            <button class="col-2" style=" width: 5px; background-color: #401201;"><a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
-</svg></a></button>
+            <button class="col-2"> Home <a href="index.php"></a></button>
             <button class="col-2"> Search <a href="search.php"></a></button>
             <button class="col-2"> Add Book <a href="addLivro.php"></a></button>                   
             <button class="col-2"> Add Author <a href="addAutor.php"></a></button>
